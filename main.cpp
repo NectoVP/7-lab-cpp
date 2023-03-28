927d40a85074b2f523e2aa2cf4b189ac45817d78
VLADPT <93070628+NectoVP@users.noreply.github.com>
2023-03-28 14:47:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-﻿#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <iostream>
 #include "circular_buffer.h"
 #include "dynamic_circular_buffer.h"
 
 int main()
 {
-	circular_buffer <int, 3> a = { 1,2,1 };
-	//a.push_back(4);
-	a.push_back(std::move(4));
-	std::vector<int> b = { 4,4,1 };
-
 	return 0;
 }
